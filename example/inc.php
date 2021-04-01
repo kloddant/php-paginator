d40e0346904154dd76a17d9f8102b17889b07045
Tony <kloddant@users.noreply.github.com>
2021-04-01 17:23:12 -0500

    Fixed indentation

--- example/inc.php
+++ example/inc.php
@@ -1,14 +1,14 @@
 <?php
 
-    if (!function_exists('dict_get')) {
+	if (!function_exists('dict_get')) {
 		function dict_get($dict, $key, $default=null) {
 			return (array_key_exists($key, $dict) ? $dict[$key] : $default);
 		}
 	}
 
-    $category = get_queried_object();
+	$category = get_queried_object();
 
-    $current_page_number = dict_get($_GET, 'page', 1);
+	$current_page_number = dict_get($_GET, 'page', 1);
 	$per_page = dict_get($_GET, 'per_page', 8);
 	$offset = ($current_page_number-1)*$per_page;
 
